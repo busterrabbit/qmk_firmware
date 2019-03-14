0156be250d7ddd2a31751bf978ac522eb58096d8
busterrabbit <48559006+busterrabbit@users.noreply.github.com>
2019-03-14 23:21:03 +0900

    Add files via upload

---
+++ keyboards/meishi/pro_v1/config.h
@@ -0,0 +1,14 @@
+#ifndef MASTER_CONFIG_H_
+#define MASTER_CONFIG_H_
+
+#include "custom_board.h"
+
+#define THIS_DEVICE_ROWS 1
+#define THIS_DEVICE_COLS 4
+
+#define MATRIX_ROW_PINS { PIN12 } // Dummy
+#define MATRIX_COL_PINS { PIN13, PIN14, PIN15, PIN16 }
+
+#define IS_LEFT_HAND  true
+
+#endif /* MASTER_CONFIG_H_ */
